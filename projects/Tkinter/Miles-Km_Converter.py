bf9a404fde8d21b0eb46fc9bcf668100b88a86e2
Manav Sharma <info.manavsharma2302@gmail.com>
2026-03-28 17:40:46 +0530

    Proper Naming

--- projects/Tkinter/Miles-Km_Converter.py
+++ projects/Tkinter/Miles-Km_Converter.py
@@ -14,25 +14,25 @@ def convert():
         result_label.config(text="Enter a valid number")
 
 # Window
-root = tk.Tk()
-root.title("KM ↔ Miles Converter")
-root.geometry("350x250")
-root.config(bg="#1e1e1e")  # dark background
+window = tk.Tk()
+window.title("KM ↔ Miles Converter")
+window.geometry("350x250")
+window.config(bg="#1e1e1e")  # dark background
 
 # Title
-title = tk.Label(root, text="KM ↔ Miles Converter",
+title = tk.Label(window, text="KM ↔ Miles Converter",
                  font=("Arial", 14, "bold"),
                  bg="#1e1e1e", fg="white")
 title.pack(pady=10)
 
 # Entry
-entry = tk.Entry(root, font=("Arial", 12), justify="center")
+entry = tk.Entry(window, font=("Arial", 12), justify="center")
 entry.pack(pady=10)
 
 # Mode (radio buttons)
 mode = tk.IntVar(value=1)
 
-frame = tk.Frame(root, bg="#1e1e1e")
+frame = tk.Frame(window, bg="#1e1e1e")
 frame.pack()
 
 rb1 = tk.Radiobutton(frame, text="KM → Miles",
@@ -48,16 +48,16 @@ rb2 = tk.Radiobutton(frame, text="Miles → KM",
 rb2.grid(row=0, column=1, padx=10)
 
 # Button
-btn = tk.Button(root, text="Convert",
+btn = tk.Button(window, text="Convert",
                 font=("Arial", 11, "bold"),
                 command=convert)
 btn.pack(pady=10)
 
 # Result
-result_label = tk.Label(root, text="",
+result_label = tk.Label(window, text="",
                         font=("Arial", 11),
                         bg="#1e1e1e", fg="white")
 result_label.pack(pady=10)
 
 # Run
-root.mainloop()
+window.mainloop()
