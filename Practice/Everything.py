63885cba84d3117dff0e39ef7f66abd5bcfdb91c
Manav Sharma <info.manavsharma2302@gmail.com>
2026-03-28 17:06:30 +0530

    practice

---
+++ Practice/Everything.py
@@ -0,0 +1,36 @@
+from cProfile import label
+from tkinter import *
+
+window = Tk()
+
+window.title("My First GUI")
+window.geometry("500x500")
+
+
+def click():
+    label.config(text=entry.get())
+
+label=Label(text="Write Some Text",font=("Times New Roman",20))
+button=Button(text="Click Me!",command=click)
+entry=Entry()
+
+
+
+label.pack()
+button.pack()
+entry.pack()
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
