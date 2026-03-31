158c076a1061dbf608d4e57dc02ea5d780f1275e
Manav Sharma <info.manavsharma2302@gmail.com>
2026-03-31 19:08:17 +0530

    shifted from text to json datafile and added exception handling

--- projects/Tkinter/Password-Manager/Main.py
+++ projects/Tkinter/Password-Manager/Main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
 
 # -------------------- DATA --------------------
 letters = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -39,24 +40,48 @@ def save():
     email_username = Email_username_entry.get()
     password = password_entry.get()
 
+    new_data={
+        website:{
+            "email":email_username,
+            "password":password
+        }
+    }
+
     if len(website) == 0 or len(email_username) == 0 or len(password) == 0:
         messagebox.showerror("Error", "Please fill all fields")
     else:
-        is_ok = messagebox.askokcancel(
-            title="Confirmation",
-            message=f"Website: {website}\nEmail: {email_username}\nPassword: {password}\n\nSave?"
-        )
+        try:
+            with open("Data.json", "r") as file:
+                data=json.load(file)
+        except FileNotFoundError:
+            with open("Data.json", "w") as file:
+                json.dump(new_data, file,indent=4)
+        else:
+            data.update(new_data)
 
-        if is_ok:
-            with open("Data.txt", "a") as file:
-                file.write(f"{website} | {email_username} | {password}\n")
+        with open("Data.json", "w") as file:
+            json.dump(data, file, indent=4)
 
-            messagebox.showinfo("Success", "Your details saved")
+        messagebox.showinfo("Success", "Your details saved")
 
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
 
 
+def search():
+    website = website_entry.get()
+    try:
+        with open("Data.json", "r") as file:
+            data=json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror("Error", "No File Found")
+    else:
+        if website in data:
+            email=data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}, Password: {password}")
+        else:
+            messagebox.showerror("Error", "No Data Found")
 # -------------------- UI --------------------
 window = Tk()
 window.title("Password Manager")
@@ -73,26 +98,29 @@ Email_username_label = Label(text="Email/Username")
 password_label = Label(text="Password")
 
 # Entries
-website_entry = Entry(width=35)
-Email_username_entry = Entry(width=35)
+website_entry = Entry(width=20)
+Email_username_entry = Entry(width=34)
 password_entry = Entry(width=20)
 
 website_entry.focus()
 
 # Buttons
 password_button = Button(text="Generate Password", command=generate_password)
-add_button = Button(text="Add", width=36, command=save)
+add_button = Button(text="Add", width=35, command=save)
+search_button = Button(text="Search",command=search)
 
 # Layout
 website_label.grid(row=1, column=0)
 Email_username_label.grid(row=2, column=0)
 password_label.grid(row=3, column=0)
 
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1, )
 Email_username_entry.grid(row=2, column=1, columnspan=2)
 password_entry.grid(row=3, column=1)
 
 password_button.grid(row=3, column=2)
 add_button.grid(row=4, column=1, columnspan=2)
+add_button.config(bg="cyan")
+search_button.grid(row=1, column=2)
 
 window.mainloop()
