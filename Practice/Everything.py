8ebcd4e6fe0064b958d0c04e4ddf2366a785d574
Manav Sharma <info.manavsharma2302@gmail.com>
2026-04-01 23:36:09 +0530

    practice on tkinker

--- Practice/Everything.py
+++ Practice/Everything.py
@@ -1,27 +1,19 @@
-from cProfile import label
-from tkinter import *
-
-window = Tk()
-
-window.title("My First GUI")
-window.geometry("500x500")
-
-
-def click():
-    label.config(text=entry.get())
-
-label=Label(text="Write Some Text",font=("Times New Roman",20))
-button=Button(text="Click Me!",command=click)
-entry=Entry()
-
-
-
-label.pack()
-button.pack()
-entry.pack()
-
-
-window.mainloop()
+# from cProfile import label
+# from tkinter import *
+#
+# window = Tk()
+#
+# window.title("My First GUI")
+# window.geometry("500x500")
+# def click():
+#     label.config(text=entry.get())
+# label=Label(text="Write Some Text",font=("Times New Roman",20))
+# button=Button(text="Click Me!",command=click)
+# entry=Entry()
+# label.pack()
+# button.pack()
+# entry.pack()
+# window.mainloop()
 
 
 
@@ -33,4 +25,8 @@ window.mainloop()
 
 
 
-window.mainloop()
+
+
+
+
+
