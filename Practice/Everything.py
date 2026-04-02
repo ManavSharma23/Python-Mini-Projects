12a589332ebb3dfc85f512cac0b54d4f29063842
Manav Sharma <info.manavsharma2302@gmail.com>
2026-04-02 20:32:06 +0530

    Practiced api and added folder for future project

--- Practice/Everything.py
+++ Practice/Everything.py
@@ -1,23 +1,27 @@
-# from cProfile import label
-# from tkinter import *
+# # from cProfile import label
+# # from tkinter import *
+# #
+# # window = Tk()
+# #
+# # window.title("My First GUI")
+# # window.geometry("500x500")
+# # def click():
+# #     label.config(text=entry.get())
+# # label=Label(text="Write Some Text",font=("Times New Roman",20))
+# # button=Button(text="Click Me!",command=click)
+# # entry=Entry()
+# # label.pack()
+# # button.pack()
+# # entry.pack()
+# # window.mainloop()
 #
-# window = Tk()
+# import requests
 #
-# window.title("My First GUI")
-# window.geometry("500x500")
-# def click():
-#     label.config(text=entry.get())
-# label=Label(text="Write Some Text",font=("Times New Roman",20))
-# button=Button(text="Click Me!",command=click)
-# entry=Entry()
-# label.pack()
-# button.pack()
-# entry.pack()
-# window.mainloop()
-
-
-
-
+# response = requests.get("http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# print(data)
 
 
 
