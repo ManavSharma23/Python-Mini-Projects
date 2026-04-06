fad3e6ff2694772a17b341b82c74c1b5472c269f
Manav Sharma <info.manavsharma2302@gmail.com>
2026-04-06 19:32:52 +0530

    Cleared The Practice Area

--- Practice/Everything.py
+++ Practice/Everything.py
@@ -1,36 +1,19 @@
-# # from cProfile import label
-# # from tkinter import *
-# #
-# # window = Tk()
-# #
-# # window.title("My First GUI")
-# # window.geometry("500x500")
-# # def click():
-# #     label.config(text=entry.get())
-# # label=Label(text="Write Some Text",font=("Times New Roman",20))
-# # button=Button(text="Click Me!",command=click)
-# # entry=Entry()
-# # label.pack()
-# # button.pack()
-# # entry.pack()
-# # window.mainloop()
-#
-# import requests
-#
-# response = requests.get("http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
-#
-# data = response.json()
-# print(data)
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
+# Dataset
+X = np.array([[1,20000],
+              [3,35000],
+              [5,50000],
+              [7,65000]])
 
+y = np.array(['Junior','Junior','Senior','Senior'])
 
+# Train model
+model = RandomForestClassifier(n_estimators=10, random_state=0)
+model.fit(X, y)
 
+# Predict for new data
+prediction = model.predict([[4,40000]])
 
-
-
-
-
-
-
-
+print("Predicted Job Level:", prediction)
