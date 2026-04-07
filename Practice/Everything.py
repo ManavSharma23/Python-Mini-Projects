9346063c197165f67ae746df6d8ac2f724249e5c
Manav Sharma <info.manavsharma2302@gmail.com>
2026-04-07 19:36:59 +0530

    Cleaned File For Practice

--- Practice/Everything.py
+++ Practice/Everything.py
@@ -1,19 +1,1 @@
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
 
-# Dataset
-X = np.array([[1,20000],
-              [3,35000],
-              [5,50000],
-              [7,65000]])
-
-y = np.array(['Junior','Junior','Senior','Senior'])
-
-# Train model
-model = RandomForestClassifier(n_estimators=10, random_state=0)
-model.fit(X, y)
-
-# Predict for new data
-prediction = model.predict([[4,40000]])
-
-print("Predicted Job Level:", prediction)
