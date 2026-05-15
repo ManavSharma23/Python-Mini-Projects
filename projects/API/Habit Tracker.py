fdaf428933d907729c17c9bbb16bdd11c49948a2
Manav Sharma <info.manavsharma2302@gmail.com>
2026-05-15 21:41:51 +0530

    added graph

--- projects/API/Habit Tracker.py
+++ projects/API/Habit Tracker.py
@@ -12,3 +12,17 @@ parameter={"token":"asdfgh754jksssdf"
 response=requests.post(url=endpoint,json=parameter)
 
 print(response.text)
+
+graph_endpoint="https://pixe.la/v1/users/maanav/graphs"
+headers = {"X-USER-TOKEN": token}
+response=requests.get(url=graph_endpoint,headers=headers)
+graph_parameter={
+        "id":"graph1",
+        "name":"Racing Graph",
+        "hours":"hrs",
+        "type":"float",
+        "color":"ajisai"
+}
+
+response=requests.post(url=graph_endpoint,json=graph_parameter,headers=headers)
+
