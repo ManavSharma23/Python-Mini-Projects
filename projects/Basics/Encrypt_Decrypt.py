4fef8c1a835783b8592143796d2e00ebc5d675b2
Manav Sharma <info.manavsharma2302@gmail.com>
2026-06-28 14:13:42 +0530

    Update Encrypt_Decrypt.py

--- projects/Basics/Encrypt_Decrypt.py
+++ projects/Basics/Encrypt_Decrypt.py
@@ -1,6 +1,6 @@
 letters=['a','b','c','d','e','f','g','h','i','j'
          ,'k','l','m','n','o','p','q','r','s','t'
-         ,'u','v','w','x','y','z',]
+         ,'u','v','w','x','y','z']
 
 def option():
     direction = int(input("What Do You Want To Do  \n"
